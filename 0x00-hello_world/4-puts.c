cf2100718a2b9ff239b3ee5e87dc534e7df7e3ce
Bouidmar88 <aubenoire@hotmail.com>
2023-07-12 23:57:27 -0700

    The puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
-int main()
+
+/**
+ * Program to use puts
+ *
+ * Not allowed to use printf
+ *
+ * Should end with valuz zero
+ */
+
+int main(void)
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);

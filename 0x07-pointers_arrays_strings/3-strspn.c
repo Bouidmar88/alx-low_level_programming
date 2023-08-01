075bf1a1e0ff030b8c4f40a075d4e8310addac6d
S <136736464+Bouidmar88@users.noreply.github.com>
2023-08-01 02:56:45 +0100

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,25 +10,25 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-		unsigned int str;
-			int x, y, z;
+	unsigned int str;
+	int x, y, z;
 
-				x = 0;
-					while (s[x] != '\0')
-							{
-										z = 0;
-												for (y = 0; accept[y] != '\0'; y++)
-															{
-																			if (s[x] == accept[y])
-																							{
-																												z = 1;
-																															}
-																					}
-														y = 0;
-																if (z == 0)
-																				break;
-																		str++;
-																				x++;
-																					}
-						return (x);
+	x = 0;
+	while (s[x] != '\0')
+	{
+		z = 0;
+		for (y = 0; accept[y] != '\0'; y++)
+		{
+			if (s[x] == accept[y])
+			{
+				z = 1;
+			}
+		}
+		y = 0;
+		if (z == 0)
+			break;
+		str++;
+		x++;
+	}
+	return (x);
 }

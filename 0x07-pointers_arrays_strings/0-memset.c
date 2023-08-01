223024ffba260607a9cf108cac4bad9a3eaaa5cc
Bouidmar88 <aubenoire@hotmail.com>
2023-07-31 18:51:11 -0700

    Tasks zero to eight

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,12 +2,13 @@
 #include <stdio.h>
 
 /**
- * _memset - function that fills memory with a constant byte
- * @s: type char pointer
- * @b: type char pointer
- * @n: type char bytes of mem pointed by s with constant byte b
- * Return: s
- */
+* _memset - function that fills memory with a constant byte
+* @s: type char pointer
+* @b: type char pointer
+* @n: type char bytes of mem pointed by s with constant byte b
+* Return: s
+*/
+
 char *_memset(char *s, char b, unsigned int n)
 {
 		unsigned int z;
--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 
 /**
- *  * _memcpy - function that copies memory to area
- *   * @dest: type char pointer
- *    * @src: type char memory area
- *     * @n: type char bytes from src to dest
- *      * Return: dest.
- *       */
+* _memcpy - function that copies memory to area
+* @dest: type char pointer
+* @src: type char memory area
+* @n: type char bytes from src to dest
+* Return: dest.
+*/
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 		unsigned int z;
